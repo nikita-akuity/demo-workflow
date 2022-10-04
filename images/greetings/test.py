1882e203e9f4f79aa74433475d4b3ef7b1a4b0b4
Nikita Kutselev <nikita@akuity.io>
2022-10-03 17:31:05 -0700

    Update unit tests for greetings

--- images/greetings/test.py
+++ images/greetings/test.py
@@ -16,19 +16,19 @@ hello B
 """]
     
     def test_default(self):
-        p = prog("--name A --name B --name 👽 --output-file /tmp/1.txt".split())
+        p = prog("--names A,B,👽 --output-file /tmp/1.txt".split())
         with open('/tmp/1.txt') as f:
             self.assertEqual(f.read(), self.OUTPUTS[0])
         os.remove('/tmp/1.txt')
 
     def test_custom(self):
-        p = prog("--greeting greetings --name A --name B --name 👽 --output-file /tmp/2.txt".split())
+        p = prog("--greeting greetings --names A,B,👽 --output-file /tmp/2.txt".split())
         with open('/tmp/2.txt') as f:
             self.assertEqual(f.read(), self.OUTPUTS[1])
         os.remove('/tmp/2.txt')
 
     def test_empty(self):
-        p = prog(['--name', '', '--name', 'B', '--output-file', '/tmp/3.txt'])
+        p = prog("--names ,B --output-file /tmp/3.txt".split())
         with open('/tmp/3.txt') as f:
             self.assertEqual(f.read(), self.OUTPUTS[2])
         os.remove('/tmp/3.txt')
